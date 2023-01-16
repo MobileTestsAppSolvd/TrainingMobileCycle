6851879e126ff030de0595adbd878efbeda05cae
German <barreragerman27@gmail.com>
2023-01-16 17:03:42 -0300

    First Commit to new Repo.

---
+++ src/main/java/com/TrainingCycleSolvdArg/carina/demo/mobile/gui/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.TrainingCycleSolvdArg.carina.demo.mobile.gui;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
