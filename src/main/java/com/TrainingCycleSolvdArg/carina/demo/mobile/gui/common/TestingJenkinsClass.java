cc45ee626f785e8480e9cb266279df6a3db36feb
MobileTestCycle <122828194+MobileTestsAppSolvd@users.noreply.github.com>
2023-01-23 15:06:54 -0300

    Delete TestingJenkinsClass.java
    This file was deleted because of installation error.

--- src/main/java/com/TrainingCycleSolvdArg/carina/demo/mobile/gui/common/TestingJenkinsClass.java
+++
@@ -1,8 +0,0 @@
-package com.TrainingCycleSolvdArg.carina.demo.mobile.gui.common;
-
-public class TestingJenkinsClass {
-
-    public static void main(String[] args) {
-        System.out.println("Hello from Jenkins!");
-    }
-}
