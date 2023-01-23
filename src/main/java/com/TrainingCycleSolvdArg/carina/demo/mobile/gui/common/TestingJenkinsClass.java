7c2e8776cf1bc7d80bb242ba647d1f6c1222c675
German <barreragerman27@gmail.com>
2023-01-23 10:49:05 -0300

    This is a second branch for testing Jenkins Integration with a Hello from Jenkins

---
+++ src/main/java/com/TrainingCycleSolvdArg/carina/demo/mobile/gui/common/TestingJenkinsClass.java
@@ -0,0 +1,8 @@
+package com.TrainingCycleSolvdArg.carina.demo.mobile.gui.common;
+
+public class TestingJenkinsClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins!");
+    }
+}
